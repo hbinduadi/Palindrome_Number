f6734e17269622698582a0e36cfc3d38d1279798
hbinduadi <42518596+hbinduadi@users.noreply.github.com>
2018-11-05 15:52:33 -0500

    Update Palindrome_Number/Program.cs

--- Palindrome_Number/Program.cs
+++ Palindrome_Number/Program.cs
@@ -10,6 +10,37 @@ namespace Palindrome_Number
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int num = 1;
+                while (num > 0)
+                {
+                    string data = num.ToString();
+                    bool isPalindrome = true;
+                    for (int i = 0; i < data.Length / 2; i++)
+                    {
+                        if (data[i] != data[(data.Length - 1) - i])
+                        {
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
+                    Console.WriteLine("The Palindrome Numbers are : ");
+                    if (isPalindrome)
+                    {
+                        Console.WriteLine("Palindrome :" + num);
+                    }
+                    num++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Caught ");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
